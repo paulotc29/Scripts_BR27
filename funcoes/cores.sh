7d69e24804e1c4fc932b8f14506b789f5a007cc0
paulo <paulotc29@gmail.com>
2020-03-05 14:49:00 -0300

    $update

---
+++ funcoes/cores.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+echo
+echo "EXEMPLO"
+echo
+echo -e "Viva o \033[01;32mLinux\033[01;37m!"
+echo
+echo
+
+echo "\033: ainda não consegui encontrar o real motivo de usar isto, mas é obrigatório para usar as cores, se trocar por outra coisa dará tudo errado;"
+echo "01( estilo): significa que nosso texto será em negrito (bold);"
+echo "32m (cor do texto): 32 representa a cor vermelha e o m indica o fim da expressão e após ele você já poderá incluir o texto que bem desejar;"
+
+
+
+echo 
+echo
+
+
+
+echo "ESTILOS"
+echo
+echo "00: Nenhum"
+echo "01: Negrito"
+echo "04: Sublinhado"
+echo "05: Piscante"
+echo "07: Reverso"
+echo "08: Oculto"
+echo 
+echo "CORES DE TEXTO"
+echo 
+echo "30: Preto"
+echo "31: Vermelho"
+echo "32: Verde"
+echo "33: Amarelo"
+echo "34: Azul"
+echo "35: Magenta (Rosa)"
+echo "36: Ciano (Azul Ciano)"
+echo "37: Branco"
+echo
+echo "CORES DE FUNDO"
+echo
+echo "40: Preto"
+echo "41: Vermelho"
+echo "42: Verde"
+echo "43: Amarelo"
+echo "44: Azul"
+echo "45: Magenta (Rosa)"
+echo "46: Ciano (Azul Ciano)"
+echo "47: Branco"
